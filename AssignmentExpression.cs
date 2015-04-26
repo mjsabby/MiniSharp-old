2877ad5ea8170cba5b8d7f3176a3aa83ca1b7fe7
mjsabby <mjsabby@gmail.com>
2015-04-25 17:02:27 -0700

    Re-organize code #2

--- AssignmentExpression.cs
+++ AssignmentExpression.cs
@@ -1,32 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LLVMSharp;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-
-namespace MiniSharpCompiler
+﻿namespace MiniSharpCompiler
 {
+	using System;
+	using LLVMSharp;
+	using Microsoft.CodeAnalysis.CSharp;
+	using Microsoft.CodeAnalysis.CSharp.Syntax;
+
 	public partial class LLVMIRGenerationVisitor
 	{
-		/// <summary>
-		/// Non variable declaration assignments
-		/// TODO: requires type conversion
-		/// </summary>
 		public override void VisitAssignmentExpression(AssignmentExpressionSyntax node)
 		{
-			if (node.Kind() != SyntaxKind.SimpleAssignmentExpression)
+			SyntaxKind kind = node.Kind();
+			this.Visit(node.Left);
+			var lvalue = this.OnlyPop(node.Left);
+
+			this.Visit(node.Right);
+			var rvalue = this.OnlyPop(node.Right);
+
+			this.valueStack.Push(rvalue);
+			this.valueStack.Push(lvalue);
+
+			switch (kind)
 			{
-				throw new Exception("only simple expressions are supported");
+				case SyntaxKind.SimpleAssignmentExpression:
+					this.valueStack.Pop();
+					break;
+				case SyntaxKind.AddAssignmentExpression:
+					this.AddExpression(node, node.Left, node.Right, visit: false);
+					break;
+				case SyntaxKind.SubtractAssignmentExpression:
+					this.SubExpression(node, node.Left, node.Right, visit: false);
+					break;
+				case SyntaxKind.MultiplyAssignmentExpression:
+					this.MulExpression(node, node.Left, node.Right, visit: false);
+					break;
+				case SyntaxKind.DivideAssignmentExpression:
+					this.DivExpression(node, node.Left, node.Right, visit: false);
+					break;
+				case SyntaxKind.ModuloAssignmentExpression:
+					this.ModExpression(node, node.Left, node.Right, visit: false);
+					break;
+				case SyntaxKind.ExclusiveOrAssignmentExpression:
+					this.XorExpression(node, node.Left, node.Right, visit: false);
+					break;
+				case SyntaxKind.AndAssignmentExpression:
+					this.BitAndExpression(node, node.Left, node.Right, visit: false);
+					break;
+				case SyntaxKind.OrAssignmentExpression:
+					this.BitOrExpression(node, node.Left, node.Right, visit: false);
+					break;
+				case SyntaxKind.LeftShiftAssignmentExpression:
+					this.ShlExpression(node, node.Left, node.Right, visit: false);
+					break;
+				case SyntaxKind.RightShiftAssignmentExpression:
+					this.ShrExpression(node, node.Left, node.Right, visit: false);
+					break;
+				default:
+					throw new Exception("Unreachable");
 			}
 
-			var currentSymbolTable = this.symbolTable.Peek().Locals;
-			var id = (IdentifierNameSyntax)node.Left;
-			var symbol = currentSymbolTable[id.Identifier.Text].Item2;
+			rvalue = this.OnlyPop(node.Right);
 
-			this.valueStack.Push(LLVM.BuildStore(this.builder, this.Pop(node.Right), symbol));
+			switch (node.Left.Kind())
+			{
+				case SyntaxKind.IdentifierName:
+					LLVM.BuildStore(this.builder, rvalue, lvalue);
+					break;
+				case SyntaxKind.ElementAccessExpression:
+					throw new NotImplementedException("ElementAccessExpression support is not yet implemented");
+				case SyntaxKind.SimpleMemberAccessExpression:
+					throw new NotImplementedException("SimpleMemberAccessExpression support is not yet implemented");
+				default:
+					throw new Exception("Unreachable");
+			}
 		}
 	}
 }
