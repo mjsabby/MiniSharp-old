5ca3faa2b262a05e4c82d1176eed92fb07d56f73
mjsabby <mjsabby@gmail.com>
2015-04-28 01:50:09 -0700

    Add invocation expression

---
+++ InvocationExpression.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniSharpCompiler
+{
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+    public partial class LLVMIRGenerationVisitor
+    {
+        public override void VisitInvocationExpression(InvocationExpressionSyntax node)
+        {
+            base.Visit(node);
+        }
+    }
+}
